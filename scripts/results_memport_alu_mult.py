399ea4e66eb15d65ded6fec7a42c7032b432974b
Ben <rbenheidorn@gmail.com>
2015-12-06 15:52:01 -0500

    Forgot to remove old files.

--- scripts/results_memport_alu_mult.py
+++
@@ -1,6 +0,0 @@
-from make_configs import makeConfigs
-from main_script import runBenchmarksOnTestSets, extractDataFromResults
-
-# Collect all of the results
-extractDataFromResults(["sim_IPC"], ["ialu_imult_memport2_test"])
-extractDataFromResults(["sim_IPC"], ["fpalu_fpmult_memport2_test"])
