267516b0496e48493bcec1c5b45f46c25f267ac5
QuangNguyen <quang319@gmail.com>
2015-12-07 15:19:19 -0500

    adding ex_ifq_speed_decode script

--- scripts/ex_ifq_speed_decode.py
+++ scripts/ex_ifq_speed_decode.py
@@ -1,16 +1,22 @@
 from make_configs import makeConfigs
 from main_script import runBenchmarksOnTestSets, extractDataFromResults
 
-makeConfigs("ifq_speed_decode", 
+
+# il1 and dl1 cache block size are fixed at ifqize * 8
+il1 = ["il1:%s:%s:1:l" %(int(1024/x),x*8) for x in [1,2,4,8,16]]
+dl1 = ["dl1:%s:%s:1:l" %(int(1024/x),x*8) for x in [1,2,4,8,16]]
+
+
+makeConfigs("ex_ifq_speed_decode", 
 	{'fetch:ifqsize': [1,2,4,8,16], 'fetch:speed': range(1,5), 'decode:width': [1,2,4,8,16],
-	 'cache:il1': ['il1:512:8:1:r', 'il1:512:16:1:r', 'il1:512:32:1:r', 'il1:512:64:1:r', 'il1:512:128:1:r'],
-	 'cache:dl1': ['dl1:512:8:1:r', 'dl1:512:16:1:r', 'dl1:512:32:1:r', 'dl1:512:64:1:r', 'dl1:512:128:1:r']
+	 'cache:il1': il1,
+	 'cache:dl1': dl1
 	 },
 	{}
 )
 
 # Run tests
-#runBenchmarksOnTestSets(["ifq_speed_decode"])
+runBenchmarksOnTestSets(["ex_ifq_speed_decode"])
 
 # Retrieve results
-#extractDataFromResults(["sim_IPC"], ["ifq_speed_decode"])
+#extractDataFromResults(["sim_IPC"], ["ex_ifq_speed_decode"])
