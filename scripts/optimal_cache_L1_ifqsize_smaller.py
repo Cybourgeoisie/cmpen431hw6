767943bf0dbae26b4e6af33e4567679b74aaebf9
Ben <rbenheidorn@gmail.com>
2015-12-07 21:27:16 -0500

    Test to ensure that the ifqsize must be greater than 2..

---
+++ scripts/optimal_cache_L1_ifqsize_smaller.py
@@ -0,0 +1,24 @@
+from make_configs import makeConfigs
+from main_script import runBenchmarksOnTestSets, extractDataFromResults
+
+il1 = ["il1:%s:%s:%s:l" %(x,(y*8),z) for x in [1024,2048,4096,8192] for y in [1,2] for z in [1,2,4]]
+
+makeConfigs("optimal_cache_L1_ifqsize_smaller", 
+	{
+		'fetch:ifqsize': [1,2], # Only certain that ifqsize > 1,2
+		'cache:il1': il1,
+		'cache:dl2': ["ul2:1024:128:2:l"]
+	},
+	{
+		'bpred': 'comb',    # Certain
+		'bpred:ras': 8,     # Only certain that RAS > 0
+		'fetch:speed': 2    # Minimal impact
+	},
+	useOptimal = True
+)
+
+# Run tests
+#runBenchmarksOnTestSets(["optimal_cache_L1_ifqsize_smaller"])
+
+# Retrieve results
+#extractDataFromResults(["sim_IPC"], ["optimal_cache_L1_ifqsize_smaller"])
