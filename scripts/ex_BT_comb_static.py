5fc38b63711eee0f8c5ddde767b69e7359e468dc
QuangNguyen <quang319@gmail.com>
2015-12-07 22:47:38 -0500

    Fixed hard-coded L2 cache with BT

--- scripts/ex_BT_comb_static.py
+++ scripts/ex_BT_comb_static.py
@@ -8,10 +8,10 @@ il1_4way = ["il1:%s:8:4:l" %(x/4) for x in [1024,2048,4096,8192]]
 
 # ul2 block size must be il1 * 2
 dl2_1way = ["dl2:%s:16:1:l" %(x) for x in [4096,8192,16384,32768,65536]]
-dl2_2way = ["dl2:%s:16:1:l" %(x/2) for x in [4096,8192,16384,32768,65536]]
-dl2_4way = ["dl2:%s:16:1:l" %(x/4) for x in [4096,8192,16384,32768,65536]]
-dl2_8way = ["dl2:%s:16:1:l" %(x/8) for x in [4096,8192,16384,32768,65536]]
-dl2_16way = ["dl2:%s:16:1:l" %(x/16) for x in [4096,8192,16384,32768,65536]]
+dl2_2way = ["dl2:%s:16:2:l" %(x/2) for x in [4096,8192,16384,32768,65536]]
+dl2_4way = ["dl2:%s:16:4:l" %(x/4) for x in [4096,8192,16384,32768,65536]]
+dl2_8way = ["dl2:%s:16:8:l" %(x/8) for x in [4096,8192,16384,32768,65536]]
+dl2_16way = ["dl2:%s:16:16:l" %(x/16) for x in [4096,8192,16384,32768,65536]]
 
 
 makeConfigs("ex_BT_comb_static", 
@@ -22,7 +22,7 @@ makeConfigs("ex_BT_comb_static",
 )
 
 # Run tests
-#runBenchmarksOnTestSets(["ex_BT_comb_static"])
+runBenchmarksOnTestSets(["ex_BT_comb_static"])
 
 # Retrieve results
 #extractDataFromResults(["sim_IPC"], ["ex_BT_comb_static"])
